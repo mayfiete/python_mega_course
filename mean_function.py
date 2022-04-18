ab18aeedc87ac970d27cdf3c7113007245d8c21d
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-04-18 10:51:56 -0400

    feat: study

---
+++ mean_function.py
@@ -0,0 +1,9 @@
+
+
+def mean (mylist): # mean function
+    the_mean = sum(mylist) / len(mylist)
+    return the_mean
+
+listz = [1, 2, 3, 4, 5]
+
+print(mean(listz))
--- my_program.py
+++ my_program.py
@@ -1,18 +1,7 @@
 
 
 # tuples are immutable
-monday_temperatures = [9.1, 8.8, 7.6]
-monday_temperatures.append(32)
 
-print(monday_temperatures[1])
+student_grades = { "Tom": 10, "Sue": 9, "Jim": 8 }
 
-print(monday_temperatures[0:2])
-
-print(monday_temperatures[-1])
-
-# provides positional access
-print(monday_temperatures.index(8.8, 1))
-
-monday_temperatures.clear() 
-
-print(monday_temperatures)
+print(student_grades["Tom"])
