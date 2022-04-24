9b5de2fd75d3ff1830c2dca00ab9252f3b87541a
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2022-04-24 08:53:06 -0400

    list comprehension

---
+++ if_conditional_list_comprehension.py
@@ -0,0 +1,7 @@
+
+
+temps = [0, 12, 34, -100] 
+
+new_temps = [temp for temp in temps if temp > 0] 
+
+print(new_temps)
---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+
+
+temps = [221, 234, 340, 100]
+
+new_temps = [temp / 10 for temp in temps]
+
+print(new_temps)
